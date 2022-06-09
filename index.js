5ed86d5dfa07097a0d6ca547b18bc0046b9baa0c
Roman Posokhov <posokhovrom@gmail.com>
2022-06-09 09:13:39 +0300

    initial commit

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ function init() {
   const form = document.querySelector('.needs-validation')
   const inputName = document.querySelectorAll('.inputName')
   const inputEmail = document.querySelector('.inputEmail')
-  const phoneCode = document.querySelector('#inputCode')
+  const inputCode = document.querySelector('#floatingSelect')
   const inputPhone = document.querySelector('.inputPhone')
-  const datalist = document.querySelector('#datalistOptions')
 
   fetch("https://ipinfo.io/json?token=c54ae77dc28792").then(
     (response) => response.json()
@@ -36,21 +35,35 @@ function init() {
     })
   })
 
+  const checkFirstLastNames = (elem, data) => {
+    if (data.length < 2) {
+      setFeedbak(elem, 'Input required at least 2 letters')
+    } else if (!data.match(/^[A-Za-zА-Яа-я]{2,35}$/gm)) {
+      setFeedbak(elem, 'Input required only letters. No numbers and symbols')
+    } else {
+      elem.style.display = 'none'
+    }
+  }
+
   inputEmail.addEventListener('input', () => {
     if (form.classList.contains('was-validated')) {
-      checkEmail(inputEmail.parentNode.lastElementChild, inputEmail.value)
+      const elem = inputEmail.parentNode.lastElementChild
+      const data = inputEmail.value
+      if (data !== data.toLowerCase()) {
+        setFeedbak(elem, 'Sorry, lower case only')
+      } else if (!data.match(/@/gm)) {
+        setFeedbak(elem, 'Email address must contain @')
+      } else if (!data.match(/@[a-z0-9.-]+\.[a-z]{2,4}$/gm)) {
+        setFeedbak(elem, 'Correct mail address must contain "." after @')
+      } else {
+        elem.style.display = 'none'
+      }
     }
   })
 
-  phoneCode.addEventListener('input', () => {
-    if (phoneCode.value[0] !== '+' || phoneCode.value.length < 1) {
-      phoneCode.value = `+${phoneCode.value}`
-    }
-    if (!phoneCode.value[phoneCode.value.length - 1].match(/^[0-9]+/g)) {
-      phoneCode.value = phoneCode.value.substring(0, phoneCode.value.length - 1);
-    }
-    if (phoneCode.value.substring(1, 4)) {
-      let codeFlag = phoneCode.value.substring(1, 4)
+  inputCode.addEventListener('change', () => {
+    const codeFlag = inputCode.options[inputCode.selectedIndex].text.substring(1, 4)
+    if (codeFlag) {
       countries.forEach(e => e.number == codeFlag && setFlag(e.country.toLowerCase()))
     }
   }
@@ -69,46 +82,22 @@ function init() {
   }
   )
 
-  const setFlag = countryCode => {
-    if (!countryCode) return
-    inputPhone.style.backgroundImage = `url(https://flagcdn.com/16x12/${countryCode}.png)`
-  }
-
   const setList = userCountry => {
+    console.log(userCountry)
     countries.forEach(e => {
-      datalist.innerHTML += `<option value=+${e.number}>`
+      inputCode.innerHTML += `<option value=${e.country} ${e.country === userCountry && 'selected'}>+${e.number}</option>`
       if (e.country === userCountry) {
-        phoneCode.value = `+${e.number}`
         setFlag(e.country.toLowerCase())
       }
     })
   }
 
-  const checkFirstLastNames = (elem, data) => {
-    if (data.length < 2) {
-      setFeedbak(elem, 'Input required at least 2 letters')
-    } else if (!data.match(/^[A-Za-zА-Яа-я]{2,35}$/gm)) {
-      setFeedbak(elem, 'Input required only letters. No numbers and symbols')
-    } else {
-      elem.style.display = 'none'
-    }
-  }
-
-  const checkEmail = (elem, data) => {
-    if (data !== data.toLowerCase()) {
-      setFeedbak(elem, 'Sorry, lower case only')
-    } else if (!data.match(/@/gm)) {
-      setFeedbak(elem, 'Email address must contain @')
-    } else if (!data.match(/@[a-z0-9.-]+\.[a-z]{2,4}$/gm)) {
-      setFeedbak(elem, 'Correct mail address must contain "." after @')
-    } else {
-      elem.style.display = 'none'
-    }
+  const setFlag = countryCode => {
+    inputPhone.style.backgroundImage = `url(https://flagcdn.com/16x12/${countryCode}.png)`
   }
 
   const setFeedbak = (elem, message) => {
     elem.style.display = 'block'
     elem.innerHTML = message;
   }
-
 }
